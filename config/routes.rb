2fd2f52a83ad1a20b0d45c56fb973a5439624552
Ulysses Spellman <uspellman@yahoo.com>
2015-02-21 19:39:36 -0500

    post controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails::application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+ resources :posts
+
+ get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 
