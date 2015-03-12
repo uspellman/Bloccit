623c83a6491273f2836d2b9ad524daa83eb0ac5f
Ulysses Spellman <uspellman@yahoo.com>
2015-03-12 02:25:26 -0400

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC')}
+  default_scope { order('created_at DESC') }
+  scope :ordered_by_title { order('title') }
+  scope :ordered_by_reverse_created_at { order('created_at') }
 end
