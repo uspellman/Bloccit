e74fda59283109ef50e46cbd1d12054ccf6dc4ba
Ulysses Spellman <uspellman@yahoo.com>
2015-03-23 22:22:01 -0400

    Authorization Assignment Completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -10,16 +10,12 @@ class PostPolicy < ApplicationPolicy
   class Scope < Scope
 
     def resolve
-      #return scope.none unless user
+      return scope.none unless user
       if user.admin? || user.moderator?
         scope.all
       else
         scope.where(user_id: user.id)
       end
-    end
-
-    def index?
-      user.admin? || user.id?
     end
     
     end
